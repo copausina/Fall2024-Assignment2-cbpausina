5f5cc87004d1464bacb8511943ed413dbf710ad7
Conlan Pausina <cbpausina@crimson.ua.edu>
2024-09-27 12:22:10 -0500

    created project and added files from zip

---
+++ Fall2024-Assigment2-cbpausina/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fall2024_Assigment2_cbpausina.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
